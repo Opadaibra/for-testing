9102f55feb393c7580a1b81c0ae6088aecc5fb72
ayman9991 <aemankh.as@gmail.com>
2020-03-29 00:54:40 +0300

    Adding Month class

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(new Today().setDay("Sunday").getDay());
+        System.out.println(new Month().setMonth("August").getMonth());
     }
 }
